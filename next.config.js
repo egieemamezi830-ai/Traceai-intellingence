c925dd15b49d879c0fec030902a8a07096729c98
egieemamezi830-ai <egieemamezi830@gmail.com>
2026-07-16 13:42:26 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
+  env: {
+    supabaseUrl: 'https://hutpmvjocklspplywxke.supabase.co',
+    supabaseKey: 'PASTE_YOUR_LONG_eyJ_KEY_HERE',
+    NEXT_PUBLIC_SUPABASE_URL: 'https://hutpmvjocklspplywxke.supabase.co',
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: 'PASTE_YOUR_LONG_eyJ_KEY_HERE',
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
